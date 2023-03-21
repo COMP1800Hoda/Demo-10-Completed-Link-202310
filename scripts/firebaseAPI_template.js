f93b5a6031285fe51f5b31bbe842c9775adf6c63
Hoda Rashedi <hrashedi1@bcit.ca>
2023-03-21 06:09:16 -0700

    firebase template cleaned up

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,7 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-  apiKey: "AIzaSyBd-daBcbDBltXlZiAfVVHkkKmuLx4BUio",
-  authDomain: "fir-fa683.firebaseapp.com",
-  projectId: "fir-fa683",
-  storageBucket: "fir-fa683.appspot.com",
-  messagingSenderId: "807252426865",
-  appId: "1:807252426865:web:1074289ca48ac8db609bd0"
+  
 };
 
 //--------------------------------------------
